d3e59db39eb51dcb5906dc3a92b4370f2b0b96a6
ibrahimkaya <kayaibrahim0@yandex.com>
2019-12-13 15:15:10 +0300

    news detail activity added

---
+++ app/src/main/java/com/example/mynewsfeed/UIcontroller/DescriptionActivity.java
@@ -0,0 +1,16 @@
+package com.example.mynewsfeed.UIcontroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mynewsfeed.R;
+
+public class DescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_description);
+    }
+}
