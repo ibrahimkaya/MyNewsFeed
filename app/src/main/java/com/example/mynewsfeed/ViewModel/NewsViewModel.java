a49494ef9241baaf9880f6603b668aa5ac5d0424
ibrahimkaya <kayaibrahim0@yandex.com>
2019-12-14 16:39:06 +0300

    news view model added

---
+++ app/src/main/java/com/example/mynewsfeed/ViewModel/NewsViewModel.java
@@ -0,0 +1,31 @@
+package com.example.mynewsfeed.ViewModel;
+
+import android.app.Application;
+
+import com.example.mynewsfeed.Repo.NewsRepository;
+import com.example.mynewsfeed.RoomDb.News;
+
+import java.util.List;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+public class NewsViewModel extends AndroidViewModel {
+
+    private NewsRepository mRepository;
+    private LiveData<List<News>> mAllNews;
+
+    public NewsViewModel(Application application){
+        super(application);
+        mRepository = new NewsRepository(application);
+        mAllNews = mRepository.getmAllNews();
+    }
+
+    public LiveData<List<News>> getmAllNews(){
+        return mAllNews;
+    }
+
+    public void insert(News...news){
+        mRepository.insert(news);
+    }
+}
